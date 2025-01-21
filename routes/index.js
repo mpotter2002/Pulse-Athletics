b4c6c3476538a4efc89532c9c5d9235d06175dec
mpotter2002 <147785323+mpotter2002@users.noreply.github.com>
2025-01-20 18:05:10 -0600

    Updated

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var personname = "Michael Potter";
+
+  res.render('index', { title: 'Express', personname: personname });
 });
 
 module.exports = router;
